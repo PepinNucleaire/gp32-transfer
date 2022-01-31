b82a620e9d0cd9076b08f2eb22a65bb0ee2bbd14
Julien Muller~ <muller.julien.02@gmail.com>
2022-01-31 23:14:46 +0100

    fix export port

--- gp32_transfer/main.py
+++ gp32_transfer/main.py
@@ -67,7 +67,7 @@ def export_to_gps(gpx_file: Path = typer.Argument(..., help="gpx file to upload"
     typer.secho(f"Have you completed the steps before? ", fg=typer.colors.BRIGHT_YELLOW)
     flag_start = typer.confirm("Is GP32 ready to receive data ?", default=True)
     if flag_start:
-        from_gpx_to_gps(gpx_file,state["port"])
+        from_gpx_to_gps(gpx_file=gpx_file,port=state["port"])
 
     typer.echo("\n" + "-" * 30)
     typer.echo("End of program")
